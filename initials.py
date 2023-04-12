28db257516ac0da9096e850b8eeabc1e927c8670
sirisha6409 <41979625+sirishaadivishnu@users.noreply.github.com>
2023-04-12 16:52:48 +0100

    my first commit

---
+++ initials.py
@@ -0,0 +1,18 @@
+# Block Letters
+# Write a Python program that displays the initials of your name in block letters using ASCII art
+#Sirisha Adivishnu
+print(" SSS ")
+print("S   S")
+print("S    ")
+print(" SSS ")
+print("    S")
+print("S   S")
+print(" SSS ")
+
+print("  A  ")
+print(" A A ")
+print("A   A")
+print("AAAAA")
+print("A   A")
+print("A   A")
+print("A   A")
