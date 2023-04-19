15ec0045198bb44282ff6bc056565d69ce1c8f1d
sirisha6409 <41979625+sirishaadivishnu@users.noreply.github.com>
2023-04-19 12:23:39 +0100

    modified passwords

---
+++ script-fazoolin.py
@@ -0,0 +1,39 @@
+# A restaurant menu organizer program 
+# Author: Sirisha Adivishnu
+class Menu:
+  def __init__(self, name, items, start_time, end_time):
+    self.name = name
+    self.items = items
+    self.start_time = start_time
+    self.end_time = end_time
+
+#Brunch menu
+brunch_items = {
+  'pancakes': 7.50, 'waffles': 9.00, 'burger': 11.00, 'home fries': 4.50, 'coffee': 1.50, 'espresso': 3.00, 'tea': 1.00, 'mimosa': 10.50, 'orange juice': 3.50
+}
+brunch_menu = Menu('Brunch', brunch_items, 1100, 1600)
+
+#Early bird menu
+early_bird_items = {
+  'salumeria plate': 8.00, 'salad and breadsticks (serves 2, no refills)': 14.00, 'pizza with quattro formaggi': 9.00, 'duck ragu': 17.50, 'mushroom ravioli (vegan)': 13.50, 'coffee': 1.50, 'espresso': 3.00,
+}
+early_bird_menu = Menu('Early Bird', early_bird_items, 1500, 1800)
+
+#Dinner menu
+dinner_items = {
+  'crostini with eggplant caponata': 13.00, 'caesar salad': 16.00, 'pizza with quattro formaggi': 11.00, 'duck ragu': 19.50, 'mushroom ravioli (vegan)': 13.50, 'coffee': 2.00, 'espresso': 3.00,
+}
+dinner_menu = Menu('Dinner Menu', dinner_items, 1700, 2300)
+
+
+
+
+
+
+
+
+
+
+  
+
+
